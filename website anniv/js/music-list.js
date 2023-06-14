116447ff81aae20fb0e79ff83e40826c44ec574c
nunupian <136609947+nunupian@users.noreply.github.com>
2023-06-14 22:26:32 +0700

    Add files via upload

---
+++ website anniv/js/music-list.js
@@ -0,0 +1,56 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Location Unknown feat. Bekka",
+    artist: "HONNE",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "You're Gonna Live Forever in Me",
+    artist: "John Mayer",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Falling Like The Stars",
+    artist: "James Arthur",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "A Thousand Years",
+    artist: "Christina Perri",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Marry You",
+    artist: "Bruno Mars",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Anything You Want",
+    artist: "Reality Club",
+    img: "music-6",
+    src: "music-6"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
